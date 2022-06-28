6d0efd08c8ccca65210419872ec73a88303ee4eb
LcMaculan <maculanluca.github@outlook.com>
2022-06-28 19:09:12 +0200

    Improved build and code structure.

--- c_sharp/run.sh
+++ c_sharp/run.sh
@@ -4,8 +4,13 @@ NUNIT_LIB=$(ls _libs | grep NUnit. | head -1)                           # ex. NU
 NUNIT_CONSOLE_RUNNER_LIB=$(ls _libs | grep NUnit.ConsoleRunner.)        # ex. NUnit.ConsoleRunner.3.15.0
 
 export MONO_PATH=$(PWD)/_libs/$NUNIT_LIB/lib/net40
-mcs $1 -target:library -r:_libs/$NUNIT_LIB/lib/net40/nunit.framework.dll -out:file_to_execute.dll
+mcs $1/$1.cs -target:library -out:$1/$1.dll
+mcs $1/test.cs -target:library \
+  -r:_libs/$NUNIT_LIB/lib/net40/nunit.framework.dll \
+  -r:$1/$1.dll \
+  -out:file_to_execute.dll
 mono --runtime=v4.0 _libs/$NUNIT_CONSOLE_RUNNER_LIB/tools/nunit3-console.exe file_to_execute.dll
 rm *.log
 rm *.xml
+rm $1/$1.dll
 rm file_to_execute.dll
